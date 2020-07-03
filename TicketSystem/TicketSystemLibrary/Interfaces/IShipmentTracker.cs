4fb70864a091f9dd4ae490a0f0cb7d1a3e1fb71c
MylesFTOP <m.fullen87@gmail.com>
2020-07-03 17:20:46 +0100

    Adding interface and mock implementation for shipment tracking

---
+++ TicketSystem/TicketSystemLibrary/Interfaces/IShipmentTracker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketSystemLibrary.Interfaces
+{
+    public interface IShipmentTracker
+    {
+        DateTime GetExpectedDeliveryDate(ShipmentModel shipment);
+    }
+}
---
+++ TicketSystem/TicketSystemLibrary/MockShipmentTracker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketSystemLibrary.Interfaces;
+
+namespace TicketSystemLibrary
+{
+    public class MockShipmentTracker : IShipmentTracker
+    {
+        public DateTime GetExpectedDeliveryDate(ShipmentModel shipment) {
+            throw new NotImplementedException();
+        }
+    }
+}
