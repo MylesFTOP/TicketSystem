9d106ca0299b462fb34277c3d81cfae42c3d567c
MylesFTOP <m.fullen87@gmail.com>
2020-07-04 18:29:41 +0100

    Adding quantity tests for updating parts quantities

---
+++ TicketSystem/TicketSystemLibrary.Tests/QuantityTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TicketSystemLibrary.Tests
+{
+    public class QuantityTests
+    {
+        private readonly PartModel part = new PartModel();
+
+        [Fact]
+        public void PartModel_AddToStockShouldIncreaseQuantity() {
+            var expected = 3;
+            part.AddToStock(3);
+            var actual = part.Quantity;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void PartModel_RemoveFromStockShouldDecreaseQuantity() {
+            part.Quantity = 3;
+            var expected = 0;
+            part.RemoveFromStock(3);
+            var actual = part.Quantity;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
