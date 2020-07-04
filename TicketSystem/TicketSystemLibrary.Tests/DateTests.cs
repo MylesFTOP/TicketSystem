7e4d9b3d1fe2c35c48e34a975a301c2cd3476e68
MylesFTOP <m.fullen87@gmail.com>
2020-07-04 17:42:30 +0100

    Setting up TicketSystemLibrary.Tests as an xUnit project and adding first unit test; adding reference to Library in Console as well

---
+++ TicketSystem/TicketSystemLibrary.Tests/DateTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TicketSystemLibrary;
+
+namespace TicketSystemLibrary.Tests
+{
+    public class DateTests
+    {
+        private readonly MockShipmentTracker shipmentTracker = new MockShipmentTracker();
+        private readonly ShipmentModel shipment = new ShipmentModel();
+
+        [Fact]
+        public void ShipmentTracker_ExpectedDeliveryDateShouldReturnADate() {
+            var actual = shipmentTracker.GetExpectedDeliveryDate(shipment);
+            Assert.IsType<DateTime>(actual);
+        }
+    }
+}
