8a9d5716a3134bbbaf9070a6b0c0045f053d23f4
MylesFTOP <m.fullen87@gmail.com>
2020-11-22 15:46:19 +0000

    Set up TicketSystemConsole as MVC application

---
+++ TicketSystem/TicketSystemConsole/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketSystemConsole.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- TicketSystem/TicketSystemConsole/Program.cs
+++ TicketSystem/TicketSystemConsole/Program.cs
@@ -1,11 +1,25 @@
-﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TicketSystemConsole
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args) {
-            throw new NotImplementedException();
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
