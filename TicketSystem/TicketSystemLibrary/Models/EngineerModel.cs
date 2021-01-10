8cb25b6c74d3c7a2f62801f43ebbbb99515940be
MylesFTOP <m.fullen87@gmail.com>
2021-01-10 16:31:03 +0000

    Extract PersonModel from EngineerModel

--- TicketSystem/TicketSystemLibrary/Models/EngineerModel.cs
+++ TicketSystem/TicketSystemLibrary/Models/EngineerModel.cs
@@ -2,26 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TicketSystemLibrary.Models;
 
 namespace TicketSystemLibrary
 {
-    public class EngineerModel
+    public class EngineerModel : PersonModel
     {
         public int EngineerId { get; private set; }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
         public string HomePostcode { get; private set; }
         public List<PartModel> PartsInStock { get; private set; } = Factory.CreatePartModelList();
         public List<PartModel> AdditionalPartsRequired { get; private set; } = Factory.CreatePartModelList();
         public List<TaskModel> ScheduledTasks { get; private set; } = Factory.CreateTaskModelList();
-
-        public void UpdateFirstName(string newFirstName) {
-            FirstName = newFirstName;
-        }
-        
-        public void UpdateLastName(string newLastName) {
-            LastName = newLastName;
-        }
         
         public void UpdateHomePostcode(string newHomePostcode) {
             HomePostcode = newHomePostcode;
---
+++ TicketSystem/TicketSystemLibrary/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketSystemLibrary.Models
+{
+    public abstract class PersonModel
+    {
+        public int PersonId { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public void UpdateFirstName(string newFirstName) {
+            FirstName = newFirstName;
+        }
+
+        public void UpdateLastName(string newLastName) {
+            LastName = newLastName;
+        }
+    }
+}
