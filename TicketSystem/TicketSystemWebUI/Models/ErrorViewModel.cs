72b895d2830c6ede4f773ca11efed03cad48b313
MylesFTOP <m.fullen87@gmail.com>
2021-03-10 22:30:10 +0000

    New Web UI project for basic interface

---
+++ TicketSystem/TicketSystemWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketSystemWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
