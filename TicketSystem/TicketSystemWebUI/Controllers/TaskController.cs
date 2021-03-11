0acf96bcb216c6c5a948df6be2fdce6c542a8e14
MylesFTOP <m.fullen87@gmail.com>
2021-03-11 00:12:22 +0000

    Starting with Tasks

---
+++ TicketSystem/TicketSystemWebUI/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystemWebUI.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ILogger<TaskController> _logger;
+
+        public TaskController(ILogger<TaskController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
