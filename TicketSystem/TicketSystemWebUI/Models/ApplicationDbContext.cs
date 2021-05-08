7712d1d4b36c7da93666745d84c20ee33b304647
MylesFTOP <m.fullen87@gmail.com>
2021-05-08 18:16:26 +0100

    Configure SQL DB connection

---
+++ TicketSystem/TicketSystemWebUI/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketSystemLibrary;
+
+namespace TicketSystemWebUI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions) {
+
+        }
+
+        public DbSet<TaskModel> TaskModels{ get; set; }
+    }
+}
--- TicketSystem/TicketSystemWebUI/Startup.cs
+++ TicketSystem/TicketSystemWebUI/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +22,11 @@ namespace TicketSystemWebUI
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<ApplicationDbContext>(
+                option => option.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")
+                    )
+                );
             services.AddControllersWithViews();
         }
 
