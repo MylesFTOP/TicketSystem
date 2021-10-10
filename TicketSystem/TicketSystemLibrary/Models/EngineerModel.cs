5af9c190cd0829d442aa9ae5e71a3829a8878a06
MylesFTOP <m.fullen87@gmail.com>
2021-10-10 22:07:45 +0100

    Finish extracting engineer stock management methods

--- TicketSystem/TicketSystemLibrary/Models/EngineerModel.cs
+++ TicketSystem/TicketSystemLibrary/Models/EngineerModel.cs
@@ -12,27 +12,21 @@ namespace TicketSystemLibrary
         public string BasePostcode { get; private set; }
         public EngineerStockModel PartStock { get; private set; } = Factory.CreateEngineerStockModel();
         public List<TaskModel> ScheduledTasks { get; private set; } = Factory.CreateTaskModelList();
-        
-        public void UpdateBasePostcode(string newBasePostcode) {
+
+        public void UpdateBasePostcode(string newBasePostcode) => 
             BasePostcode = newBasePostcode;
-        }
 
-        public void AddPartsToStock(List<PartModel> partsSent) {
+        public void AddPartsToStock(List<PartModel> partsSent) => 
             PartStock.AddPartsToStock(partsSent);
-        }
 
-        public void RemovePartsFromStock(List<PartModel> partsUsed) {
+        public void RemovePartsFromStock(List<PartModel> partsUsed) => 
             PartStock.RemovePartsFromStock(partsUsed);
-        }
 
-        public List<PartModel> DetermineRequiredPartsForScheduledTasks() => 
-            ScheduledTasks.SelectMany(x => x.PartsRequired).ToList();
+        public List<PartModel> DetermineRequiredPartsForScheduledTasks() =>
+            PartStock.DetermineRequiredPartsForScheduledTasks(ScheduledTasks);
 
-        public void DetermineAdditionalPartsRequired() {
-            var additionalPartsRequired = DetermineRequiredPartsForScheduledTasks();
-            PartStock.AdditionalPartsRequired = additionalPartsRequired.UpdateStockQuantities(
-                PartStock.PartsInStock.InvertStockQuantities());
-        }
+        public void DetermineAdditionalPartsRequired() => 
+            PartStock.DetermineAdditionalPartsRequired(ScheduledTasks);
 
         public void CompleteTaskForEngineer(TaskModel currentTask) {
             ScheduledTasks.RemoveAll(x => x.TaskId == currentTask.TaskId);
--- TicketSystem/TicketSystemLibrary/Models/EngineerStockModel.cs
+++ TicketSystem/TicketSystemLibrary/Models/EngineerStockModel.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TicketSystemLibrary
 {
     public class EngineerStockModel
     {
+        public int OwningEngineerId { get; set; }
         public List<PartModel> PartsInStock { get; private set; } = Factory.CreatePartModelList();
         public List<PartModel> AdditionalPartsRequired { get; set; } = Factory.CreatePartModelList();
 
-        public void AddPartsToStock(List<PartModel> partsSent) {
+        public void AddPartsToStock(List<PartModel> partsSent) => 
             PartsInStock.UpdateStockQuantities(partsSent);
-        }
 
         public void RemovePartsFromStock(List<PartModel> partsUsed) {
             partsUsed.InvertStockQuantities();
             PartsInStock.UpdateStockQuantities(partsUsed);
+        }
+
+        public List<PartModel> DetermineRequiredPartsForScheduledTasks(List<TaskModel> tasks) =>
+            tasks.SelectMany(x => x.PartsRequired).ToList();
+
+        public void DetermineAdditionalPartsRequired(List<TaskModel> tasks) {
+            var additionalPartsRequired = DetermineRequiredPartsForScheduledTasks(tasks);
+            AdditionalPartsRequired = additionalPartsRequired.UpdateStockQuantities(
+                PartsInStock.InvertStockQuantities());
         }
     }
 }
