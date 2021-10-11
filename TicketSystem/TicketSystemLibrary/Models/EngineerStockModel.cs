a8e087b92e20a0e6b293d626360898ceca47716a
MylesFTOP <m.fullen87@gmail.com>
2021-10-11 01:03:35 +0100

    Abstract to engineer stock being a stock location

--- TicketSystem/TicketSystemLibrary/Models/EngineerStockModel.cs
+++ TicketSystem/TicketSystemLibrary/Models/EngineerStockModel.cs
@@ -1,23 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace TicketSystemLibrary
 {
-    public class EngineerStockModel
+    public class EngineerStockModel : StockLocationModel
     {
         public int OwningEngineerId { get; set; }
-        public List<PartModel> PartsInStock { get; private set; } = Factory.CreatePartModelList();
         public List<PartModel> AdditionalPartsRequired { get; set; } = Factory.CreatePartModelList();
-
-        public void AddPartsToStock(List<PartModel> partsSent) => 
-            PartsInStock.UpdateStockQuantities(partsSent);
-
-        public void RemovePartsFromStock(List<PartModel> partsUsed) {
-            partsUsed.InvertStockQuantities();
-            PartsInStock.UpdateStockQuantities(partsUsed);
-        }
 
         public void UpdateAdditionalPartsRequired(List<TaskModel> tasks) {
             var additionalPartsRequired = DetermineRequiredPartsForScheduledTasks(tasks);
---
+++ TicketSystem/TicketSystemLibrary/Models/StockLocationModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TicketSystemLibrary
+{
+    public class StockLocationModel
+    {
+        public List<PartModel> PartsInStock { get; private set; } = Factory.CreatePartModelList();
+
+        public void AddPartsToStock(List<PartModel> partsSent) =>
+            PartsInStock.UpdateStockQuantities(partsSent);
+
+        public void RemovePartsFromStock(List<PartModel> partsUsed) {
+            partsUsed.InvertStockQuantities();
+            PartsInStock.UpdateStockQuantities(partsUsed);
+        }
+    }
+}
